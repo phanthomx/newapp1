d5eeff1aea3f2f00ff8e86723b9679119e0d9217
phanthomx <aryanjbagwe@gmail.com>
2024-05-08 16:57:59 +0530

    jj

--- App.js
+++ App.js
@@ -1,20 +1,60 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View , Button, TextInput} from 'react-native';
 
 export default function App() {
+  const [enteredgoaltext , setenteredGoalText] = useState( ' ');
+  function goalInputHandler(enteredText) {
+    // console.log("The entered text is " + enteredText);
+    setenteredGoalText(enteredText);
+    
+  }
+  function addgoalhandler() {}
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder='your course goal!' onChangeText={goalInputHandler}/>
+        <Button title="Add Goal "  onPress={addgoalhandler}/>
+        
+      </View>
+      <View style={styles.goalsContainer}>
+        <Text>List of goal ... </Text>
+      </View>
     </View>
+    
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  appContainer:{
+    flex:1,
+
+    paddingTop:50,
+    paddingHorizontal: 16,
+
+
   },
+  inputContainer:{
+    flex:1,
+    flexDirection :'row',
+    justifyContent: "space-between",
+    alignItems:"center",
+    marginBottom:24,
+    borderBottomWidth:1,
+    borderBottomColor:'#c8ced3',
+
+  },
+  textInput:{
+    borderWidth:1,
+    borderColor: "#cccccc",
+    width:"70%",
+    marginRight:8,
+    padding: 8,
+
+  },
+  goalsContainer:{
+    flex:5,
+
+  },
+
 });
